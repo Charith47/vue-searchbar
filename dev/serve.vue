7bc1aebc83e0382fb3ec44803f63d93b117dd356
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-04-23 17:20:03 +0530

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import SearchBar from '@/search-bar.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    SearchBar
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <search-bar />
+  </div>
+</template>
---
+++ src/search-bar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="widget">
+    <input v-model="query" type="text">
+  </div>
+</template>
+
+<script>
+
+export default /*#__PURE__*/{
+  name: 'SearchBar',
+  data() {
+    return {
+      query: '',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
