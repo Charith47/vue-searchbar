b6f27a4710dd68774a510170ff147ad380965fbb
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-04-24 11:11:35 +0530

    added dynamic debounce

--- src/search-bar.vue
+++ src/search-bar.vue
@@ -1,20 +1,38 @@
 <template>
   <div class="widget">
-    <input v-model="query" type="text">
+    <label for="search-bar"></label>
+    <input type="text" name="search-bar" @input="debouncedInput" />
+    <p>{{ query }}</p>
   </div>
 </template>
 
 <script>
+import debounce from "lodash.debounce";
 
-export default /*#__PURE__*/{
-  name: 'SearchBar',
+export default /*#__PURE__*/ {
+  props: {
+    delay: {
+      type: Number,
+      default: 350,
+    },
+  },
+  name: "SearchBar",
   data() {
     return {
-      query: '',
+      query: "",
+      results: [],
     };
+  },
+  methods: {
+    enter(e) {
+      this.query = e.target.value;
+      // call api
+    },
+  },
+  computed: {
+    debouncedInput() {
+      return debounce(this.enter, this.delay);
+    },
   },
 };
 </script>
-
-<style scoped>
-</style>
