15fb1d75e042e2397561f5824e174bffd4a32d28
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-04-26 07:18:10 +0530

    added esc shortcut

--- src/search-bar.vue
+++ src/search-bar.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <label for="search-bar"></label>
-    <input ref="input" type="text" name="search-bar" @input="debouncedInput" />
+    <input
+      ref="input"
+      type="text"
+      name="search-bar"
+      @input="debouncedInput"
+      @focus="hasFocus = true"
+      @blur="hasFocus = false"
+    />
     <p>{{ query }}</p>
   </div>
 </template>
@@ -20,21 +27,30 @@ export default /*#__PURE__*/ {
   data() {
     return {
       query: "",
+      hasFocus: false,
     };
   },
   created() {
-    // add event listner for '/' shortcut
+    // add event listner for shortcuts
+    // '/' for focusing on searchbar
+    // ESC to clear searchbar and unfocus
     document.addEventListener("keydown", this.handleDocumentShortcuts);
   },
   methods: {
     enter(e) {
       this.query = e.target.value;
+      this.$emit("search", this.query);
     },
     handleDocumentShortcuts(e) {
-      console.log("key pressed", e);
       if (e.key === "/" && !this.hasFocus) {
         e.preventDefault();
         this.$refs.input.focus();
+      } else if (e.key === "Escape" && this.hasFocus) {
+        e.preventDefault();
+        this.$nextTick(() => {
+          this.$refs.input.value = "";
+          this.$refs.input.blur();
+        });
       }
     },
   },
